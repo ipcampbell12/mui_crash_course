c85ea0bd49ab0d6f6022af67063610900035c86e
ipcampbell12 <ipcampbell12@gmail.com>
2023-01-10 10:47:42 -0800

    tuesday commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from "@mui/material"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { orange, red } from "@mui/material/colors"
+import { styled } from "@mui/material/styles"
 
-function App() {
+const theme = createTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      main: "#CCC"
+    },
+    secondary: {
+      main: orange[500]
+    },
+    myCustomColor: {
+      main: red[400],
+      superDark: red[800],
+      superLight: red[100],
+    },
+  },
+  typography: {
+    myVariant: {
+      fontSize: "6rem",
+      color: orange[500]
+    }
+  }
+});
+
+
+
+function App(props) {
+
+  const MyCustomButton = styled(Button)({
+    padding: 10
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Typography variant="myVariant"> This is my app</Typography>
+        <Typography sx={{ color: "myCustomColor.main" }} variant="h6"> Hit the like button!</Typography>
+        <Button sx={{ p: 2 }} variant="contained" color="secondary"> Hello from Mui</Button>
+
+      </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+
